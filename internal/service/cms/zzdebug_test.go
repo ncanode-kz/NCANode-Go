36c0ece5cf0093debaa1c2ff9094ecf88c81eb43
Malik Z. <cmalikz.h@gmail.com>
2026-08-20 23:16:30 +0500

    debug: temporary logging to diagnose CI signing/OCSP failures

---
+++ internal/service/cms/zzdebug_test.go
@@ -0,0 +1,27 @@
+package cms
+
+import (
+	"encoding/base64"
+	"errors"
+	"testing"
+
+	"github.com/ncanode-kz/NCANode-Go/internal/dto"
+	"github.com/ncanode-kz/NCANode-Go/internal/testutil"
+)
+
+func TestZZDebugSignError(t *testing.T) {
+	a := testutil.NewApp(t)
+
+	data := base64.StdEncoding.EncodeToString([]byte("cms handler test"))
+
+	_, err := sign(a, dto.CmsCreateRequest{
+		Data:    data,
+		Signers: []dto.SignerRequest{signerReq(t, "individual/valid/individual_valid.p12")},
+	}, false)
+	if err != nil {
+		t.Logf("top error: %v", err)
+		t.Logf("unwrapped: %v", errors.Unwrap(err))
+	} else {
+		t.Log("no error")
+	}
+}
